4b248d4c40c7797abd70bf03a06152e42c76955d
Lonz8576 <Lonz8576@users.noreply.github.com>
2024-05-15 00:00:14 -0400

    JS added for AI
    tested well, copy text btn was taken out

--- src/script.js
+++ src/script.js
@@ -1,17 +1,3 @@
-let copyButtonElement = document.querySelector("#copy-btn");
-let poemAreaCopy = document.querySelector("#poem-area").innerText;
-
-copyButtonElement.addEventListener("click", function() {
-    let tempText = document.createElement("textArea");
-    tempText.value = poemAreaCopy;
-    document.body.appendChild(tempText);
-
-    tempText.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempText);
-    alert("Text Copied 😊");
-
-});
 
 
 
@@ -19,16 +5,34 @@ copyButtonElement.addEventListener("click", function() {
 
 
 
-
-function generatePoem(event){
- event.preventDefault();
-new Typewriter("#poem-area", {
-  strings:"Forgotten by pickers. Not forgotten— they couldn't reach it.",
+function displayPoem(response){
+ new Typewriter("#poem-area", {
+  strings: response.data.answer,
   autoStart: true,
   delay: 1.5,
 
 });
 }
 
+
+
+
+function generatePoem(event){
+ event.preventDefault();
+
+ let instructions = document.querySelector("#prompt-input");
+let apiKey = "2e99ddt6a7e37f7c164ob09d070ab380";
+let prompt = `Generate a poem written in English by Sappho about ${instructions.value}`;
+let context = "Please include one of the user instruction:You are a scholar in Archaic Greek poems and love to write short poems. Your mission is to generate a poem from this time period in the English language.";
+let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiUrl).then(displayPoem);
+
+}
+
+
 let poemElement = document.querySelector("#poem-form");
 poemElement.addEventListener("submit", generatePoem);
+
+
+
